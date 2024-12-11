079cadef94b77cf69e7df849aa015b8f77dba453
Marta Sánchez <marta.msp97@gmail.com>
2024-12-11 13:51:56 +0100

    Se ha avanzado en la pagina home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  reserva: FormGroup = new FormGroup(
+    {
+      llegada: new FormControl(),
+      salida: new FormControl(),
+      num_personas: new FormControl(),
+    })
 
+
+  onSubmit() { }
 }
