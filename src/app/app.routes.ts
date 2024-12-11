9b67df1686ee3db85d77aac32cdbe7e323cef384
Juan Castaneda <jccastanedacamargo@outlook.com>
2024-12-11 11:10:49 +0100

    Se crea Componente de las habitaciones con sus respectivos estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HabitacionesComponent } from './pages/habitaciones/habitaciones.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'habitaciones', component: HabitacionesComponent }
+];
