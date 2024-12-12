84887cb1ec0f7dd4b6616259cab331b8b143ac64
nuria <nuria.fernandez.val@gmail.com>
2024-12-12 15:15:45 +0100

    creación del dashboard, sidebar, reservas y habitaciones del crm

---
+++ src/app/pages/crm-reservas/crm-reservas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crm-reservas',
+  standalone: true,
+  imports: [],
+  templateUrl: './crm-reservas.component.html',
+  styleUrl: './crm-reservas.component.css'
+})
+export class CrmReservasComponent {
+
+}
