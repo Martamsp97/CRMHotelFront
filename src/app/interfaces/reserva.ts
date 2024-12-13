9ba1b6434ed51a04af6e2b2ee5a89f88f0018829
nuria <nuria.fernandez.val@gmail.com>
2024-12-13 13:47:56 +0100

    creadas la interfaz y el servicio de reservas, funciona el sacar todas las reservas, se está trabajando en los filtros

---
+++ src/app/interfaces/reserva.ts
@@ -0,0 +1,18 @@
+export interface Reserva {
+    id: number;
+    fecha_entrada: Date;
+    fecha_salida: Date;
+    puntuacion: number;
+    usuarios_id: number;
+    num_personas: number;
+    regimen: string;
+    tipo_cancelacion: string;
+    aparcamiento: boolean;
+    desayuno: boolean;
+    spa: boolean;
+    gimnasio: boolean;
+    piscina: boolean;
+    precio: number;
+    metodo_pago: string;
+    estado: string;
+}
