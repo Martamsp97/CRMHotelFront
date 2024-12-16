f6feab082378c50b7b5508fd9d99a5697469bce9
nuria <nuria.fernandez.val@gmail.com>
2024-12-16 09:53:04 +0100

    html del detalle de reserva

--- src/app/pages/crm-detalle-reserva/crm-detalle-reserva.component.ts
+++ src/app/pages/crm-detalle-reserva/crm-detalle-reserva.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Reserva } from '../../interfaces/reserva';
 
 @Component({
   selector: 'app-crm-detalle-reserva',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './crm-detalle-reserva.component.css'
 })
 export class CrmDetalleReservaComponent {
+
+  reserva: Reserva | null = null
 
 }
