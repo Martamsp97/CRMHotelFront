8a467db99b67b0516712d983524af069d5f689e8
Juan Castaneda <jccastanedacamargo@outlook.com>
2024-12-18 09:55:16 +0100

    Se modifica services de habitaciones con los datos correspondientes en el Back

--- src/app/interfaces/habitacion.interface.ts
+++ src/app/interfaces/habitacion.interface.ts
@@ -3,13 +3,13 @@ export default interface Habitacion {
     id: number;
     piso: number;
     puerta: number;
-    mascotas: number;
+    mascotas: boolean;
     num_camas: string;
     categoria: string;
     precio: number;
     tamanho: string;
     vista: string;
-    cocina: number;
+    cocina: boolean
 }
 
 export interface Details {
